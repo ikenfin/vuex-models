6d2514288d7895730f31f111b91d18be65558436
ikenfin <ikenfin@gmail.com>
2018-07-05 03:01:40 +0400

    :tada: initial

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+// rollup.config.js
+import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
+import commonjs from 'rollup-plugin-commonjs';
+import eslint from 'rollup-plugin-eslint';
+import autoExternal from 'rollup-plugin-auto-external';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/bundle.esm.js',
+      format: 'es',
+      sourceMap: true
+    }
+  ],
+  plugins: [
+    autoExternal(),
+    commonjs(),
+    eslint(),
+    babel(babelrc({
+      addExternalHelpersPlugin: false
+    }))
+  ]
+};
